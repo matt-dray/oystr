c4eb6ddf7079e0ef9705ae903ba8320ecf164c63
matt-dray <mwdray@gmail.com>
2019-07-30 19:55:46 +0100

    Add comments, add if-stop, expand docs

--- R/read.R
+++ R/read.R
@@ -1,26 +1,36 @@
 #' Read Oyster-history CSV files from a folder
 #'
 #' @param path A string describing a filepath to a folder of CSVs
-#' @return A dataframe object.
+#' @return A data.frame object.
 #' @export
+
 oy_read <- function(path) {
 
+  if(!is.character(path)) {
+    stop(
+      "You must provide a character string to the path argument.\n",
+      "You provided an object of class ",
+      class(path)[1])
+  }
+
+  # Extract the CSV filenames
   oy_files <- list.files(
-    path = path,
-    pattern = "*.csv",
-    full.names = TRUE
+    path = path,        # filpath specified by user
+    pattern = "*.csv",  # must be a csv file
+    full.names = TRUE   # returns full path
   )
 
+  # Read and bind the outputs to data frame
   oy_df <- do.call(
-    "rbind",
-    lapply(
-      oy_files,
-      function(x) read.csv(
-        file = x,
-        stringsAsFactors = FALSE,
-        skip = 1,
-        header = TRUE,
-        na.strings = ""
+    "rbind",                        # bind the rows of the output
+    lapply(                         # map to a list
+      oy_files,                     # for each file path...
+      function(x) read.csv(         # ...read the CSV file
+        file = x,                   # where x is each file path
+        stringsAsFactors = FALSE,   # strings to characters, not factors
+        skip = 1,                   # ignore blank first row
+        header = TRUE,              # the first (non-blank) row is headers
+        na.strings = c("", "<NA>")  # replace blanks with NA
       )
     )
   )
