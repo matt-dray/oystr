697a45e6577d9ead1c5c0f0fd59c633bd691cb3e
matt-dray <mwdray@gmail.com>
2019-07-31 22:15:36 +0100

    Add flow to discard non-Oyster statements, add oy_read description

--- R/read.R
+++ R/read.R
@@ -1,5 +1,9 @@
-#' Read Oyster-history CSV files from a folder
+#' Read Oyster journey history files
 #'
+#' @description Read one or more Oyster journey history files from a single folder
+#'     and combine them. Assumes journey histories are raw CSV files as received
+#'     by email from Transport for London. Ignores files that are not identified
+#'     as Oyster history journey files.
 #' @param path A string describing a filepath to a folder of CSVs
 #' @return A data.frame object.
 #' @export
@@ -9,21 +13,20 @@ oy_read <- function(path) {
   if(!is.character(path)) {
     stop(
       "You must provide a character string to the path argument.\n",
-      "You provided an object of class ",
-      class(path)[1])
+      "You provided an object of class ", class(path)[1])
   }
 
   # Extract the CSV filenames
-  oy_files <- list.files(
+  data_paths <- list.files(
     path = path,        # filpath specified by user
     pattern = "*.csv",  # must be a csv file
     full.names = TRUE   # returns full path
   )
 
   # Read the CSV files into a list
-  oy_list_all <- lapply(          # map to a list
-    oy_files,                     # for each file path...
-    function(x) read.csv(         # ...read the CSV file
+  data_list_all <- lapply(        # map to a list
+    X = data_paths,               # for each file path...
+    FUN = function(x) read.csv(   # ...read the CSV file
       file = x,                   # where x is each file path
       stringsAsFactors = FALSE,   # strings to characters, not factors
       skip = 1,                   # ignore blank first row
@@ -32,20 +35,40 @@ oy_read <- function(path) {
     )
   )
 
+  names(data_list_all) <- data_paths
+
   # Filter for elements with conditions of a TfL Oyster history statement
-  # TODO: filter for elements that have the expected column names
-  oy_list_filter <- Filter(function(x) length(x) == 8, oy_list)
+
+  # First create logical vector of whether column names in file match expected
+  # names, then filter the list where TRUE
+  suppressWarnings(
+    element_logical <- sapply(
+      X = names(data_list_all),
+      FUN = function(x) all(names(data_list_all[[x]]) == c(
+        "Date", "Start.Time", "End.Time", "Journey.Action",
+        "Charge", "Credit", "Balance", "Note"
+      )
+      )
+    )
+  )
+
+  data_list_subset <- data_list_all[element_logical]
 
   # Warning that elements were removed
-  if(length(oy_list_filter) < length(oy_list_all)) {
-    count_removed <- length(oy_list_all) - length(oy_list_filter)
-    cat(paste(count_removed, "element(s) weren't in the expected format and were discarded\n\n"))
+  if(length(data_list_subset) < length(data_list_all)) {
+    count_removed <- length(data_list_all) - length(data_list_subset)
+    if(count_removed == 1) {
+      cat(paste(count_removed, "element wasn't in the expected format and was discarded\n\n"))
+    } else {
+      cat(paste(count_removed, "elements weren't in the expected format and were discarded\n\n"))
+    }
   }
 
   # Bind the rows from each list element (data frame)
-  oy_df <- do.call("rbind", oy_list_filter)
+  data_df <- do.call("rbind", data_list_subset)
+  rownames(data_df) <- c()
 
   # Return the object
-  return(oy_df)
+  return(data_df)
 
 }
