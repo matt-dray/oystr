9f6e54ffbcfe7797efde8d91a05b4661ac2d10fd
matt-dray <mwdray@gmail.com>
2019-07-31 08:28:51 +0100

    Make reference to hexadecimal in oy_cols description

--- R/read.R
+++ R/read.R
@@ -20,21 +20,32 @@ oy_read <- function(path) {
     full.names = TRUE   # returns full path
   )
 
-  # Read and bind the outputs to data frame
-  oy_df <- do.call(
-    "rbind",                        # bind the rows of the output
-    lapply(                         # map to a list
-      oy_files,                     # for each file path...
-      function(x) read.csv(         # ...read the CSV file
-        file = x,                   # where x is each file path
-        stringsAsFactors = FALSE,   # strings to characters, not factors
-        skip = 1,                   # ignore blank first row
-        header = TRUE,              # the first (non-blank) row is headers
-        na.strings = c("", "<NA>")  # replace blanks with NA
-      )
+  # Read the CSV files into a list
+  oy_list_all <- lapply(          # map to a list
+    oy_files,                     # for each file path...
+    function(x) read.csv(         # ...read the CSV file
+      file = x,                   # where x is each file path
+      stringsAsFactors = FALSE,   # strings to characters, not factors
+      skip = 1,                   # ignore blank first row
+      header = TRUE,              # the first (non-blank) row is headers
+      na.strings = ""             # replace blanks with NA
     )
   )
 
+  # Filter for elements with conditions of a TfL Oyster history statement
+  # TODO: filter for elements that have the expected column names
+  oy_list_filter <- Filter(function(x) length(x) == 8, oy_list)
+
+  # Warning that elements were removed
+  if(length(oy_list_filter) < length(oy_list_all)) {
+    count_removed <- length(oy_list_all) - length(oy_list_filter)
+    cat(paste(count_removed, "element(s) weren't in the expected format and were discarded\n\n"))
+  }
+
+  # Bind the rows from each list element (data frame)
+  oy_df <- do.call("rbind", oy_list_filter)
+
+  # Return the object
   return(oy_df)
 
 }
