a69e57e6fc51b07faf0130433f7bc14b87ea91f8
matt-dray <mwdray@gmail.com>
2019-08-14 19:25:10 +0100

    Add initial oy_col() tests

--- tests/testthat/test-colour.R
+++ tests/testthat/test-colour.R
@@ -1,7 +1,36 @@
 context("test-colour")
 
-test_that("colour output is a named character vector", {
-  a <- oystr::oy_cols()
-  expect_equal(class(a), "character")
+test_that("valid numeric input vector returns vector", {
+  expect_identical(class(oy_cols(1)), "character")
+  expect_identical(class(oy_cols(1, 2)), "character")
 })
 
+test_that("valid character input vector returns vector", {
+  expect_identical(class(oy_cols("line_circle")), "character")
+  expect_identical(class(oy_cols("line_circle", "line_victoria")), "character")
+})
+
+# test_that("valid mixed input vector returns vector", {
+#   # TODO: mixed input (numeric and character) should yield result
+#   # expect_identical(class(oy_cols("1", "line_victoria")), "character")
+# })
+
+test_that("non-vector input returns error", {
+  test_list <- list(x = 1:3, y = 1:3)
+  expect_error(oy_cols(test_list))
+  test_df <- data.frame(x = 1:3, y = 1:3)
+  expect_error(oy_cols(test_df))
+  # TODO: multidimensional arrays and matrices should fail
+})
+
+test_that("invalid indices fail", {
+  expect_error(oy_cols(length(oy_cols()) + 1))
+  expect_error(oy_cols(999))
+  # TODO: negative values should cause an error
+})
+
+test_that("invalid names fail", {
+  expect_identical(class(oy_cols("line_circle")), "character")
+  expect_identical(class(oy_cols("line_circle", "line_victoria")), "character")
+  expect_identical(class(oy_cols(c("line_circle", "line_victoria"))), "character")
+})
