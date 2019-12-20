a60c176d6dd8ff07938a64830724f56e0e51b2ac
Matt Dray <mwdray@gmail.com>
2019-12-20 12:57:19 +0000

    Slight tidy-up of roxygen

--- R/data.R
+++ R/data.R
@@ -4,15 +4,10 @@
 #' originally as a CSV from Transport for London and read with \code{"oystr::oy_read()"}.
 #'
 #' @docType data
-#'
 #' @usage data(journeys_read)
-#'
 #' @format An object of class \code{"data.frame"}.
-#'
 #' @keywords datasets
-#'
 #' @source Data generated and received originally from (\href{https://tfl.gov.uk/}{Transport for London}).
-#'
 #' @examples
 #' data(journeys_read)
 "journeys_read"
@@ -24,15 +19,10 @@
 #' and cleaned with \code{"oystr::oy_read()"}.
 #'
 #' @docType data
-#'
 #' @usage data(journeys_clean)
-#'
 #' @format An object of class \code{"data.frame"}.
-#'
 #' @keywords datasets
-#'
 #' @source Data generated and received originally from (\href{https://tfl.gov.uk/}{Transport for London}).
-#'
 #' @examples
 #' data(journeys_clean)
 "journeys_clean"
